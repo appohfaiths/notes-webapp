9b440e6fd4b74aa710b2b38b3e990c85fdfbf1ec
KodjoCode <appohfaiths@gmail.com>
2024-01-13 22:29:44 +0000

    patch: display updated time on view page

--- frontend/todo-app/src/pages/View.tsx
+++ frontend/todo-app/src/pages/View.tsx
@@ -6,14 +6,14 @@ import { ConvertUnixTimeToDate } from '../utils/utils';
 
 export default function View(): React.JSX.Element {
     const {state} = useLocation();
-    const { title, body, created_at, updated_at } = state;
+    const { title, body, updated_at } = state;
     return (
         <Layout>
             <main className='container mx-auto'>
                 <BackToHome />
                 <h1 className='text-2xl text-red-500'>View Note</h1>
                 <CTA type='view' title={title} />
-                <p>last updated: { updated_at == 0 ? ConvertUnixTimeToDate(created_at): ConvertUnixTimeToDate(updated_at)}</p>
+                <p>last updated: {ConvertUnixTimeToDate(updated_at)}</p>
                 <p>{body}</p>
             </main>
         </Layout>
