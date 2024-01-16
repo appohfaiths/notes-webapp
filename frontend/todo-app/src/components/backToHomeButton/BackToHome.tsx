f43a0899806bf14a7a8c9c3a40718dd4265d275b
KodjoCode <appohfaiths@gmail.com>
2024-01-16 12:40:22 +0000

    feat: add icon to back to home button

--- frontend/todo-app/src/components/backToHomeButton/BackToHome.tsx
+++ frontend/todo-app/src/components/backToHomeButton/BackToHome.tsx
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom'
+import { FaArrowLeft } from 'react-icons/fa'
 
 export const BackToHome = () => {
     const navigator = useNavigate()
     const handleClick = () => {navigator('/')}
   return (
-    <button onClick={handleClick}>Back to Home</button>
+    <button className='flex gap-2 items-center rounded-lg m-4 p-2 bg-primary-blue text-secondary-white' onClick={handleClick}><FaArrowLeft /> Back to home </button>
   )
 }
