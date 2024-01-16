a12efe6220a74ffeb38ebbbb3b027efd3b5cbf61
KodjoCode <appohfaiths@gmail.com>
2024-01-16 12:57:56 +0000

    patch: ui improvements

--- frontend/todo-app/src/components/previewCard/PreviewCard.tsx
+++ frontend/todo-app/src/components/previewCard/PreviewCard.tsx
@@ -9,9 +9,9 @@ export const PreviewCard: React.FC<PreviewCardProps> = ({ title, body, created_t
         navigator('/view', { state: { title, body, created_time, note_id, updated_time } })
     }
   return (
-      <article className='rounded-2xl shadow-lg flex flex-col w-72 bg-secondary-white text-secondary-black cursor-pointer' onClick={handleClick}>
-          <h3 className='text-center'>{title}</h3>
-          <p className='pl-2 border-t-2 border-b-2'>{body}</p>
+      <article className='rounded-2xl shadow-lg flex flex-col w-72 bg-secondary-white text-secondary-black cursor-pointer border-2 border-slate-400' onClick={handleClick}>
+          <h3 className='text-center line-clamp-1 overflow-ellipsis overflow-hidden'>{title}</h3>
+          <p className='pl-2 border-t-2 border-b-2 line-clamp-2 overflow-ellipsis overflow-hidden'>{body}</p>
           <p className='pl-2'>created: {ConvertUnixTimeToDate(created_time)}</p>
     </article>
   )
